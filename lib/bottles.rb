9f8871046c7d9caa149333594300ae8fe1af86e6
Tomaz Zlender <tomaz.zlender@gmail.com>
2022-08-23 14:37:42 +0200

    pass test_a_couple_verses

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,42 +1,33 @@
 class Bottles
   def verse(number)
-    case number
-    when 99
-      "99 bottles of beer on the wall, " +
-        "99 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "98 bottles of beer on the wall.\n"
-    when 3
-      "3 bottles of beer on the wall, " +
-        "3 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "2 bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, " +
-        "2 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "1 bottle of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, " +
-        "1 bottle of beer.\n" +
-        "Take it down and pass it around, " +
-        "no more bottles of beer on the wall.\n"
-    when 0
+    if number == 0
       "No more bottles of beer on the wall, " +
         "no more bottles of beer.\n" +
         "Go to the store and buy some more, " +
         "99 bottles of beer on the wall.\n"
+    elsif number == 1
+      "1 bottle of beer on the wall, " +
+        "1 bottle of beer.\n" +
+        "Take it down and pass it around, " +
+        "no more bottles of beer on the wall.\n"
+    elsif number == 2
+      "2 bottles of beer on the wall, " +
+        "2 bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "1 bottle of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, " +
+        "#{number} bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "#{number - 1} bottles of beer on the wall.\n"
     end
-    # "#{pluralize_bottle(number)} of beer on the wall, " +
-    #   "#{pluralize_bottle(number)} of beer.\n" +
-    #   (
-    #     if number == 1
-    #       "Take it down and pass it around, "
-    #     else
-    #       "Take one down and pass it around, "
-    #     end
-    #   ) +
-    #   "#{pluralize_bottle(number - 1)} of beer on the wall.\n"
+  end
+
+  def verses(first, last)
+    verses = (last..first).to_a.map do |number|
+      verse(number)
+    end
+    verses.reverse.join("\n")
   end
 
   private
